7530a71f18145876f32c05ea3ee7e8a7f5b98d54
Botez Mario Cosmin <botezmariocosmin@gmail.com>
2023-05-15 10:44:03 +0200

    0.1 - classe griglia

--- X&O_Console/X&O_Console.cpp
+++ X&O_Console/X&O_Console.cpp
@@ -7,60 +7,18 @@
 
 using namespace std;
 
-void creaTabella()
-{
-    cout << "   A   B   C  \n";
-    cout << "1    |   |   \n";
-    cout << "  ---|---|---\n";
-    cout << "2    |   |   \n";
-    cout << "  ---|---|---\n";
-    cout << "3    |   |   \n";
-}
-
-int main()
-{
-    int scelta;
-    string coordinata;
-
-    string simboli[9];
-    string coordinateUsate[9];
-
-    Griglia g(simboli, coordinateUsate);
-
-    cout << "Benvenuto in X&O!\n";
-    creaTabella();
-    cout << "\n";
-    cout << "Inserire una coordinata:\n";
-    cin >> coordinata;
-    cout << coordinata;
-    /*
-    cin >> scelta;
-    switch (scelta) {
-    case 1:
-        // code block
-        break;
-    case 2:
-        // code block
-        break;
-    default:
-        // code block
-    }*/
-}
-
-
-
-class Griglia{
+class Griglia {
 private: string simboli[9];
        string coordinateUsate[9];
 
-public:     
+public:
 
     void setSimboli(string simb[9]) {
         for (int i = 0; i < 9; i++) {
             simboli[i] = simb[i];
         }
     }
-      
+
     string* getSimboli() {
         return simboli;
     }
@@ -82,15 +40,55 @@ public:
     }
 
     void Rimuovi() {
-        
+
     }
     void Crea()
     {
-
+        cout << "   A   B   C  \n";
+        cout << "1    |   |    \n";
+        cout << "  ---|---|--- \n";
+        cout << "2    |   |    \n";
+        cout << "  ---|---|--- \n";
+        cout << "3    |   |    \n";
     }
     void AggiungiSimbolo(string simb, string coord)
     {
 
     }
 };
+
+int main()
+{
+    int scelta;
+    string coordinata;
+
+    string simboli[9];
+    string coordinateUsate[9];
+
+    Griglia g(simboli, coordinateUsate);
+
+    cout << "Benvenuto in X&O!\n";
+    cout << "\n";
+    g.Crea();
+    cout << "\n";
+    cout << "Inserire una coordinata:\n";
+    cin >> coordinata;
+    cout << coordinata;
+    /*
+    cin >> scelta;
+    switch (scelta) {
+    case 1:
+        // code block
+        break;
+    case 2:
+        // code block
+        break;
+    default:
+        // code block
+    }*/
+}
+
+
+
+
 
