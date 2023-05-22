715a3b08470ee6f5c88dd801f7dcd9771c7500da
BotezMarioCosmin <botezmariocosmin@gmil.com>
2023-05-22 10:23:01 +0200

    0.7 - aggiunta eliminazione statistiche

--- X&O_Console/X&O_Console.cpp
+++ X&O_Console/X&O_Console.cpp
@@ -11,7 +11,7 @@ using namespace std;
 void SalvaStatistiche(int vittorieComputer, int vittorieUtente, int parita) {
     ofstream file("statistiche.txt");
     if (file.is_open()) {
-        file << vittorieComputer << " " << vittorieUtente << " " << parita;
+        file << vittorieComputer << ";" << vittorieUtente << ";" << parita;
         file.close();
     }
     else {
@@ -19,10 +19,32 @@ void SalvaStatistiche(int vittorieComputer, int vittorieUtente, int parita) {
     }
 }
 
+string Split(string s, string delim, int j)
+{
+    string array[100];
+    int x = 0;
+    int i = 0;
+    for (i = 0; s.find(delim) != -1; i++)
+    {
+        x = s.find(delim);
+        string token = s.substr(0, x);
+        array[i] = token;
+        s = s.substr((x + 1), s.length());
+    }
+
+    array[i] = s;
+    return array[j];
+
+}
+
 void CaricaStatistiche(int& vittorieComputer, int& vittorieUtente, int& parita) {
     ifstream file("statistiche.txt");
     if (file.is_open()) {
-        file >> vittorieComputer >> vittorieUtente >> parita;
+        string s;
+        cin >> s;
+        vittorieComputer = stoi(Split(s, ";", 0));
+        vittorieUtente = stoi(Split(s, ";", 0));
+        parita = stoi(Split(s, ";", 0));
         file.close();
     }
     else {
@@ -32,11 +54,23 @@ void CaricaStatistiche(int& vittorieComputer, int& vittorieUtente, int& parita) 
     }
 }
 
+void EliminaStatistiche()
+{
+    if (remove("statistiche.txt") == 0)
+    {
+        cout << "File statistiche.txt eliminato correttamente." << endl;
+    }
+    else
+    {
+        cout << "Impossibile eliminare il file statistiche.txt." << endl;
+    }
+}
+
 class Griglia {
-private: string simboli[9] 
-           = { " "," "," ", " ", " ", " ", " ", " ", " " };
+private: string simboli[9]
+    = { " "," "," ", " ", " ", " ", " ", " ", " " };
        string coordinate[9]
-           = { "A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"};
+           = { "A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3" };
        string coordinate2[9]
            = { "a1", "b1", "c1", "a2", "b2", "c2", "a3", "b3", "c3" };
 
@@ -60,11 +94,11 @@ public:
     void Crea()
     {
         cout << "   A   B   C  \n";
-        cout << "1  "+simboli[0] +" | "+simboli[1] + " | "+simboli[2] + "  \n";
+        cout << "1  " + simboli[0] + " | " + simboli[1] + " | " + simboli[2] + "  \n";
         cout << "  ---|---|--- \n";
-        cout << "2  "+simboli[3] +" | "+simboli[4] + " | "+simboli[5] + "  \n";
+        cout << "2  " + simboli[3] + " | " + simboli[4] + " | " + simboli[5] + "  \n";
         cout << "  ---|---|--- \n";
-        cout << "3  "+simboli[6] +" | "+simboli[7] + " | "+simboli[8] + " \n";
+        cout << "3  " + simboli[6] + " | " + simboli[7] + " | " + simboli[8] + " \n";
     }
     void AggiungiSimbolo(string simb, string coor)
     {
@@ -125,7 +159,7 @@ public:
         }
         return false;
     }
-    string ControllaVittoria(string symbol) 
+    string ControllaVittoria(string symbol)
     {
         if (simboli[0] == symbol && simboli[1] == symbol && simboli[2] == symbol) {
             return symbol;
@@ -170,9 +204,10 @@ int main()
         cout << "\n";
         cout << "Cosa vuoi fare?\n";
         cout << "\n1. Gioca.";
-        cout << "\n2. Visualizza statistiche di gioco.\n";
+        cout << "\n2. Visualizza statistiche di gioco.";
+        cout << "\n3. Elimina cronologia partite (statistiche).\n";
         cin >> scelta;
-        if (scelta != 1 || scelta != 2)
+        if (scelta != 1 && scelta != 2 && scelta != 3)
         {
             cout << "Scelta non valida! (Premi un tasto per riprovare)";
         }
@@ -182,6 +217,17 @@ int main()
         string simboli[9] = { " "," "," ", " ", " ", " ", " ", " ", " " };
 
         Griglia g(simboli);
+
+        int tot = vittorieUtente + vittorieComputer + parita;
+        int perc;
+        if (tot == 0)
+        {
+            perc = 0;
+        }
+        else
+        {
+            perc = 100 * vittorieUtente / tot;
+        }
 
         switch (scelta) {
         case 1:
@@ -247,14 +293,30 @@ int main()
             break;
         case 2:
             system("CLS");
-            int tot = vittorieUtente + vittorieComputer + parita;
-            int perc = 100 * vittorieUtente / tot;
-            cout << "Totale partite: "<<(tot);
+            cout << "Totale partite: " << (tot);
             cout << "\nVittorie Utente: " << vittorieUtente << " (" << perc << "%)";
             cout << "\nVittorie Computer: " << vittorieComputer;
             cout << "\nParita': " << parita;
             cout << "\nPremi un tasto per tornare indietro.";
-            break;        
+            break;
+        case 3:
+            system("CLS");
+            cout << "Sei sicuro? S/N\n";
+            string sino;
+            cin >> sino;
+            if (sino == "S" || sino == "s")
+            {
+                EliminaStatistiche();
+            }
+            else if (sino == "N" || sino == "n")
+            {
+                cout << "\nPremi un tasto per tornare indietro.";
+            }
+            else
+            {
+                cout << "";
+            }
+            break;
         }
 
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
